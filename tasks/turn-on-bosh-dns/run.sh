241bf82bcbdf6c096de4f9ad2f3705005f59dcf0
Guillermo Kardolus <gkardolus@pivotal.io>
2018-11-12 16:57:36 -0500

    Update edge-dev pipeline to use bbl-state per env
    - Remove unecessary opsfiles
    - add rim-dev and fringe-dev to cf-starget
    [#161755427]
    
    Co-authored-by: Danny Joyce <djoyce@pivotal.io>

--- tasks/turn-on-bosh-dns/run.sh
+++ tasks/turn-on-bosh-dns/run.sh
@@ -2,9 +2,12 @@
 set -eux
 set -o pipefail
 
+trap "pkill -f ssh" EXIT
+
 set +x
-cd "bbl-state/${BBL_STATE_DIR}"
-eval "$(bbl print-env)"
-bosh -n update-runtime-config bosh-deployment/runtime-configs/dns.yml --name dns
+pushd "bbl-state/${BBL_STATE_DIR}"
+  eval "$(bbl print-env)"
+popd
 set -x
 
+bosh -n update-runtime-config bosh-deployment/runtime-configs/dns.yml --name dns
