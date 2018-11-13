9e4cd890239d1ab43ea78acafa78527fb7c0144f
Danny Joyce <djoyce@pivotal.io>
2018-11-13 10:09:34 -0500

    Make clean up env script delete hwc-buildpack and install after bosh-deploy
    Co-authored-by: An Yu <anyu@pivotal.io>

--- tasks/clean-up-env-buildpacks/run.sh
+++ tasks/clean-up-env-buildpacks/run.sh
@@ -10,35 +10,37 @@ cf_password=$(credhub get -n /bosh-${ENV_NAME}/cf/cf_admin_password -j | jq -r .
 target="api.$ENV_NAME.buildpacks-gcp.ci.cf-app.com"
 cf api "$target" --skip-ssl-validation || (sleep 4 && cf api "$target" --skip-ssl-validation)
 
-if [ "$?" == "1" ]; then #This is run before deploying the environment so this can fail
+# This is run before deploying the environment so this can fail
+if [ "$?" == "1" ]; then
   exit 0
 fi
 
 cf auth admin "$cf_password" || (sleep 4 && cf auth admin "$cf_password")
 set -x
 
-custom_buildpacks=$(cf buildpacks | tail -n +4 | cut -d ' ' -f1 | grep -v 'hwc_buildpack\|apt_buildpack\|binary_buildpack\|credhub_buildpack\|dotnet_core_buildpack\|go_buildpack\|nginx_buildpack\|nodejs_buildpack\|php_buildpack\|python_buildpack\|r_buildpack\|ruby_buildpack\|staticfile_buildpack\|java_buildpack' || true)
+# We don't have the hwc-buildpack in this list because cf-deployment installs it as nil-stack and we want to
+# delete it here and replace it later.
+custom_buildpacks=$(cf buildpacks | tail -n +4 | cut -d ' ' -f1 | grep -v 'apt_buildpack\|binary_buildpack\|credhub_buildpack\|dotnet_core_buildpack\|go_buildpack\|nginx_buildpack\|nodejs_buildpack\|php_buildpack\|python_buildpack\|r_buildpack\|ruby_buildpack\|staticfile_buildpack\|java_buildpack' || true)
 null_buildpacks=$(cf buildpacks | tail -n +4 | grep -v 'cflinuxfs2\|cflinuxfs3\|windows2012R2\|windows2016' | cut -d ' ' -f1 || true)
 
 for bp in $custom_buildpacks $null_buildpacks; do
   cf delete-buildpack "$bp" -f
 done
 
-for windows_stack in windows2012R2 windows2016; do
-  hwc_buildpacks=$(cf buildpacks | tail -n +4 | grep "hwc_buildpack" | grep $windows_stack) || true
-
-  if [[ "$hwc_buildpacks" == "" ]]; then
+if [ "$INSTALL_STACK_ASSOC_HWC_BPS" = true ] ; then
+  for windows_stack in windows2012R2 windows2016; do
     pushd hwc-buildpack
-      export GOPATH="$PWD"
-      export GOBIN=$PWD/.bin
-      export PATH=$GOBIN:$PATH
-      git submodule update --init --recursive
-      (cd src/*/vendor/github.com/cloudfoundry/libbuildpack/packager/buildpack-packager && go install)
+        export GOPATH="$PWD"
+        export GOBIN=$PWD/.bin
+        export PATH=$GOBIN:$PATH
+        git submodule update --init --recursive
+        (cd src/*/vendor/github.com/cloudfoundry/libbuildpack/packager/buildpack-packager && go install)
 
-      buildpack-packager build -stack "$windows_stack"
-      cf create-buildpack hwc_buildpack hwc_buildpack-*.zip 999
-      rm hwc_buildpack-*.zip
+        buildpack-packager build -stack "$windows_stack"
+        cf create-buildpack hwc_buildpack hwc_buildpack-*.zip 999
+        rm hwc_buildpack-*.zip
     popd
-  fi
-done
+  done
+fi
+
 
